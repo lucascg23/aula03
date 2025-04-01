6385ee173c830129128f2eb23bc9ec200d07383a
lucass <lukascg23@gmail.com>
2025-04-01 08:50:27 -0300

    realizei o exercicio 1

---
+++ tipo de dados.py
@@ -0,0 +1,4 @@
+nome = (input("informe seu nome "))
+idade = int (input("informe sua idade "))
+salario = float (input("informe seu salário "))
+print(f"seu nome é {nome} você tem {idade}  anos e recebe um salário de R${salario}")
