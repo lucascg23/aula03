989b303a5878d21f6ca2a3b2e367fffd7f1246cb
lucass <lukascg23@gmail.com>
2025-04-01 09:18:44 -0300

    calculei o salario e o aumento

--- tipo de dados.py
+++ tipo de dados.py
@@ -1,4 +1,9 @@
 nome = (input("informe seu nome "))
 idade = int (input("informe sua idade "))
 salario = float (input("informe seu salário "))
-print(f"seu nome é {nome} você tem {idade}  anos e recebe um salário de R${salario}")
+porcentagem = float (input("quantos % o seu salario aumentou: "))
+amais =  salario*porcentagem/100
+nsalario = salario*porcentagem/100+salario
+print(f"seu nome é {nome} você tem {idade}  anos e recebe um salário de R${salario:.2f} você agora vai receber um aumento de {amais:.2f}"
+      f" e o seu novo salario vai ser de {nsalario:.2f}")
+
