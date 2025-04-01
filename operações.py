bc876834dcd3a030e0177fb7217152d9bb6d227e
lucass <lukascg23@gmail.com>
2025-04-01 09:40:56 -0300

    4 operações matematicas

---
+++ operações.py
@@ -0,0 +1,10 @@
+numero1 = int (input("me informa um numero ai paizão: "))
+numero2 = int (input("me informa outro numero de pitbull ai: "))
+
+adicao = numero1 + numero2
+subtracao = numero1 - numero2
+divisao = numero1/numero2
+multiplicacao = numero1*numero2
+
+print( f"a soma desses numeros vai ser de {adicao}\n a subtração vai ser de {subtracao}\n a divisão vai ser de "
+       f"{divisao}\n e a multiplicação vai ser de {multiplicacao}")
