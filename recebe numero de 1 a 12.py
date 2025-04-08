59ace02bab104bb0cb2d933e3a7835f9e60d86e1
lucass <lukascg23@gmail.com>
2025-04-08 09:30:53 -0300

    numero referente ao mês

---
+++ recebe numero de 1 a 12.py
@@ -0,0 +1,32 @@
+n=int (input("escreva um numero "))
+
+if n<1 or n>12:
+
+    print("esse numero ai nao passa paizao")
+else:
+    if  n==1:
+        print("janeiro ")
+    elif n==2:
+        print("fev")
+    elif n==3:
+        print("março")
+    elif n==4:
+        print("abril")
+    elif n==5:
+        print("maio")
+    elif n==6:
+        print("junho")
+    elif n==7:
+        print("julho")
+    elif n==8:
+        print("agosto")
+    elif n==9:
+        print("setembro")
+    elif n==10:
+        print("outubro")
+    elif n==11:
+        print("novembro")
+    elif n==12:
+        print("dezembro")
+
+
